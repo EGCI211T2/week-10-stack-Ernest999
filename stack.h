ab01ae825b6df69bd1d903f359b00bfdc0db5e49
Ernest999 <chonnakan.son@student.mahidol.edu>
2025-12-04 08:45:59 +0000

    finish

--- stack.h
+++ stack.h
@@ -1,52 +1,67 @@
 #ifndef stack_h
 #define stack_h
 #include "node.h"
-
 class Stack {
-    NodePtr top;
-    int size;
+	NodePtr top;
+	int size;
 public:
     void push(int);
     int pop();
+    int get_size();
     Stack();
     ~Stack();
 };
 
+
 void Stack::push(int x){
-    NodePtr new_node = new NODE(x);  // Create new node
-    if (new_node) {
-        new_node->set_next(top);  // Point the new node's 'next' to the current top
-        top = new_node;        // Set the new node as the top of the stack
-        size++;                // Increment the size of the stack
-    }
+  NodePtr new_node=new NODE(x);
+  if(new_node){//if can create new node success fully
+            // Left missing for exercises…
+            new_node->set_next(top);
+            top = new_node;
+            size++;
+  }
+         // Left missing for exercises…
+    
+}
+
+int Stack::get_size(){
+    return size;
 }
 
 int Stack::pop(){
-    if (top != nullptr) {
-
-
-
-
-    NodePtr t = top;
-    int value = t->get_value();   
-    top = top->next;              
-    delete t;                    
-    size--;                     
-    return value;
-}
-cout << "Empty stack ."<<endl;
-return 0;
-}
-Stack::Stack() {
-    top = nullptr;
-    size = 0;
-}
-
-Stack::~Stack(){
-    cout<<"Clear the stack<<endl";
-    while (top != nullptr) {
-        pop();
+    if(top == NULL){
+        //cout<<"empty stack"<<endl;
+        return -1;
     }
+    
+    NodePtr t=top;
+       
+    int value;
+    value=t->get_value();
+    top = t->get_next();
+        // Left missing part for exercises
+    delete t;
+    size--;
+    //cout<<"pop "<<value<<endl;
+    return value;
+        //be careful of the empty stack!!!
+    
 }
+
+Stack::Stack(){
+    //initialize stack
+    top = NULL;
+    size = 0;
+    
+}
+Stack::~Stack(){
+    //cout<<"clear the stack"<<endl;
+    while(top != NULL){
+        pop();
+    } 
+    
+}
+
 
 #endif
